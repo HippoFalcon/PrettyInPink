ff5ae842f4d4c14e246737764e90cceef0093182
Brendan Hoppe <277467@bwls.net>
2017-01-26 13:18:02 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+N = 50;
+
+function setup(){
+ createCanvas( windowWidth , windowHeight );
+
+//noStroke();
+ 
+}
+
+function draw(){
+ 
+ for( n = 0 ; n < N ; n++ ){
+ //BLACK L
+ fill( 255,0,125 );
+  stroke(255,0,255);
+   ellipse( random( -windowWidth , windowHeight ) , random( windowHeight , 0) ,
+   random( 1 , 200));
+  //WHITE R
+  fill( 255,255,255);
+  stroke(255,0,125);
+   ellipse( random( windowWidth , windowHeight ) , random( windowHeight , 0)
+   ,random( 1 , 200));
+  //WHITE L
+  fill( 255,255,255);
+  stroke(255,0,125);
+  ellipse( random( -windowWidth , windowHeight ) , random( windowHeight , 0) ,
+  random( 1 , 20));
+  //BLACK R
+  fill( 255,0,125 );
+  stroke(255,0,255);
+  ellipse( random( windowWidth , windowHeight ) , random( windowHeight , 0) ,
+  random( 1 , 20));
+ }
+ 
+ 
+}
